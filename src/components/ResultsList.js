effa0f1cda9c9e6a5a2f988f9f63acb735c06804
Shania Dicen <shania@fullsuite.ph>
2020-02-07 14:23:46 +0800

    Add ResultsShowScreen and Results Detail

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { withNavigation } from 'react-navigation';
+import ResultsDetail from './ResultsDetail'
 
-const ResultsList = ({ title, results }) => {
+const ResultsList = ({ title, results, navigation }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
-      <Text>Results: {results.length}</Text>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={results}
+        keyExtractor={(result) => result.id}
+        renderItem={({ item }) => {
+          return(
+            <TouchableOpacity onPress={() => navigation.navigate('ResultsShow', { id: item.id })}>
+              <ResultsDetail result={item} />
+            </TouchableOpacity>
+          );
+        }}
+      />
     </View>
   );
 };
@@ -13,8 +27,13 @@ const ResultsList = ({ title, results }) => {
 const styles = StyleSheet.create({
   title: {
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 5
+  },
+  container: {
+    marginBottom: 10
   }
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState }from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
@@ -14,30 +14,29 @@ const SearchScreen = () => {
     });
   };
 
-  console.log(results);
-
   return (
-    <View>
+    <>
       <SearchBar
         term={term}
         onTermChange={setTerm}
         onTermSubmit={() => searchApi(term)}
       />
-        {errorMessage ? <Text>{errorMessage}</Text> : null}
-    <Text>We have found {results.length} results</Text>
-    <ResultsList
-      results={filterResultsByPrice('$')}
-      title="Cost Effective"
-    />
-    <ResultsList
-      results={filterResultsByPrice('$$')}
-      title="Bit Pricier"
-    />
-    <ResultsList
-      results={filterResultsByPrice('$$$')}
-      title="Big Spender"
-    />
-    </View>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <ScrollView>
+        <ResultsList
+          results={filterResultsByPrice('$')}
+          title="Cost Effective"
+        />
+        <ResultsList
+          results={filterResultsByPrice('$$')}
+          title="Bit Pricier"
+        />
+        <ResultsList
+          results={filterResultsByPrice('$$$')}
+          title="Big Spender"
+        />
+      </ScrollView>
+    </>
   );
 };
 
